df92ca731b67844eada4bfc9a53a9e0862964f4f
youmu178 <youmu178@sina.com>
2018-06-01 15:39:32 +0800

    no message

--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
+// The Vue build veirsion to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import Routers from './router/index.js'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(iView)
+
+// The routing configuration
+const RouterConfig = {
+  routes: Routers
+}
+
+const router = new VueRouter(RouterConfig)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  router: router,
+  render: h => h(App)
 })

f551ef3cb60a9f5780ca78a9f8644445d6fae595
youzehong <youzehong@9fgroup.cn>
2018-06-07 12:37:59 +0800

    no message

--- front/src/main.js
+++ front/src/main.js
@@ -7,12 +7,14 @@ import router from './router'
 // import Routers from './src/router/index.js'
 import iView from 'iview'
 import ElementUI from 'element-ui'
+import VCharts from 'v-charts'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'iview/dist/styles/iview.css'
 
 // Vue.use(VueRouter)
 Vue.use(iView)
 Vue.use(ElementUI)
+Vue.use(VCharts)
 
 Vue.config.productionTip = false
 
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/views/hello/HelloWorld'
 import Main from '@/views/Main'
+import TrendingToday from '@/views/TrendingToday'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: Main
+    },
+    {
+      path: '/',
+      name: 'TrendingToday',
+      component: TrendingToday
     },
     {
       path: '/hello',

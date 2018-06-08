5d9d9233845b5cc3f7ecde8f7ce7411ff9beb3c3
youzehong <youzehong@9fgroup.cn>
2018-06-08 18:17:18 +0800

    集成v-echarts

--- front/src/components/TrendingToday.vue
+++ front/src/components/TrendingToday.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>今日趋势</div>
+
+    <ve-line :data="chartData"></ve-line>
+
 </template>
+<script>
+export default {
+  data () {
+    return {
+      chartData: {
+        columns: ['类型', '今日', '昨日'],
+        rows: [
+          { '类型': 'M0', '今日': 13, '昨日': 93 },
+          { '类型': 'M1', '今日': 35, '昨日': 32 },
+          { '类型': 'M2', '今日': 23, '昨日': 26 },
+          { '类型': 'M3', '今日': 72, '昨日': 14 },
+          { '类型': 'M4', '今日': 37, '昨日': 49 }
+        ]
+      }
+    }
+  }
+}
+</script>

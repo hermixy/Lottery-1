cc0cb114c500db0fd0405a6669a35ccde0f94e3f
youzehong <youzehong@9fgroup.cn>
2018-06-12 18:36:59 +0800

    集成SQLAlchemy

--- Lottery.py
+++ Lottery.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, url_for, render_template, redirect, request, jsonify
+from flask import Flask, url_for, render_template, redirect, request, jsonify, Response
+from flask_sqlalchemy import SQLAlchemy
 import DataCombinations
+import OpenData
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/lotterys.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
+class OpenNumber(db.Model):
+    __tablename__ = 'open_numbers'
+    id = db.Column(db.Integer, primary_key=True)
+    data_period = db.Column(db.String(20))
+    data_award = db.Column(db.String(50))
+
+    def __init__(self, data_period, data_award):
+        self.data_period = data_period
+        self.data_award = data_award
+
+    # def __repr__(self):
+    #     return '<期号 %r>' % self.data_period
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/lottery/getOpenData', methods=['POST'])
+def get_open_info():
+    if request.method == 'POST':
+        date = request.form['date']
+        numberJson = OpenData.getOpenNumbers(date)
+        return Response(numberJson, mimetype='application/json')
+    return '暂无数据'
 
 @app.route('/lottery', methods=['POST'])
 def get_numbers():
@@ -34,7 +59,6 @@ def get_numbers():
             m4 = DataCombinations.getM4()
             return return2Json(m4)
     return '暂无数据'
-
 
 def return2Json(result):
     return jsonify({"data": str(result)})
--- OpenData.py
+++ OpenData.py
@@ -5,12 +5,15 @@ import requests
 # import sched
 import time
 from datetime import datetime
+import json
 from apscheduler.schedulers.blocking import BlockingScheduler
+from Lottery import OpenNumber, db
 
 session = HTMLSession()
 scheduler = BlockingScheduler()
 
 count = 0
+
 
 def getData():
     global count
@@ -18,8 +21,10 @@ def getData():
     content = response.html.find('section.main', first=True)
     body = content.find('tbody')
     itemDicts = dict()
-    countLists = []
-    countLists.clear()
+    numberDicts = dict()
+    numberDicts.clear()
+    # countLists = []
+    # countLists.clear()
     for tr in body:
 
         for tds in tr.find('td'):
@@ -30,25 +35,57 @@ def getData():
                     period = td.attrs['data-period']
                     award = td.attrs['data-award']
                     # print("序号：" + td.text + " 期号：" + period + " 开奖号码：" + award)
-                    countLists.append(td.text)
+                    # countLists.append(td.text)
                     itemDicts[period] = award
                 except KeyError as e:
                     print('except: ', e)
                 # finally:
                 #     print('finally')
     sortItemDict = sorted(itemDicts.keys(), reverse=False)
-    countLists.sort(reverse=False)
+    # print(sortItemDict)
+    # print(itemDicts)
+    # countLists.sort(reverse=False)
     # print(countLists)
     for key in sortItemDict:
-        print("期号：", key, " 开奖号码：", itemDicts[key])
+        openNumber = OpenNumber(key, itemDicts[key])
+        db.session.add(openNumber)
+        # print("期号：", key, " 开奖号码：", itemDicts[key])
+        numberDicts[key] = itemDicts[key]
         if not itemDicts[key]:
             count = key[-2:]
             break
+    db.session.commit()
 
+    print(numberDicts)
     print(count)
     count = str(count)
     # if count == str(87):
     #     scheduler.remove_job('job_index')
+    return numberDicts
+
+
+class DataAward(object):
+    def __init__(self, data_period, data_award):
+        self.data_period = data_period
+        self.data_award = data_award
+
+
+numberDict = []
+
+
+def getOpenNumbers(date):
+    numberDict.clear()
+    list2json = {}
+    openNumber = OpenNumber.query.all()
+    # print(type(openNumber))
+    for item in openNumber:
+        # print(type(item))
+        dataAward = DataAward(item.data_period, item.data_award)
+        numberDict.append(dataAward)
+        # print(json.dumps(dataAward, default=lambda obj: obj.__dict__))
+    list2json["listData"] = numberDict
+    jsonRes = json.dumps(list2json, default=lambda obj: obj.__dict__)
+    return jsonRes
 
 
 if __name__ == '__main__':

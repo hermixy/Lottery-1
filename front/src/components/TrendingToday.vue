a94e9835ebe96d9999c55ca166438880c443b22f
youzehong <youmu178@sina.com>
2018-07-05 17:51:58 +0800

    大小比奇偶比图表

--- front/src/components/TrendingToday.vue
+++ front/src/components/TrendingToday.vue
@@ -1,10 +1,11 @@
 <template>
-  <Row :gutter="30" :style="{marginTop: '20px'}">
-    <Col :md="24" :lg="12">
-      <Card>
+<div>
+  <Row :gutter="20" :style="{marginTop: '20px'}">
+    <Col :md="24" :lg="24">
+      <Card id="card">
         <p slot="title">
             <Icon type="ios-film-outline"></Icon>
-            近10日出现次数
+            近15日出现次数
         </p>
         <ve-line
           :data="chartData"
@@ -15,11 +16,48 @@
         </ve-line>
       </Card>
     </Col>
-
-    <Col :md="24" :lg="12">
+  </Row>
+  <Row :gutter="20" :style="{marginTop: '20px'}">
+    <Col :sm="24" :md="11" :lg="9">
+      <Card>
+        <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            昨日大小比出现次数
+        </p>
+        <ve-pie :data="chartData_daxiaobi_zuori" ref="chartData_daxiaobi_zuori_ref"></ve-pie>
+      </Card>
+    </Col>
+    <Col :sm="24" :md="13" :lg="15">
+     <Card>
+         <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            近5日大小比出现次数
+        </p>
+        <ve-histogram :data="chartData_daxiaobi_5ri" ref="chartData_daxiaobi_5ri_ref"></ve-histogram>
+      </Card>
     </Col>
   </Row>
-
+  <Row :gutter="20" :style="{marginTop: '20px'}">
+    <Col :sm="24" :md="11" :lg="9">
+      <Card>
+        <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            昨日奇偶比出现次数
+        </p>
+        <ve-pie :data="chartData_qioubi_zuori" ref="chartData_qioubi_zuori_ref"></ve-pie>
+      </Card>
+    </Col>
+    <Col :sm="24" :md="13" :lg="15">
+     <Card>
+         <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            近5日奇偶比出现次数
+        </p>
+        <ve-histogram :data="chartData_qioubi_5ri" ref="chartData_qioubi_5ri_ref"></ve-histogram>
+      </Card>
+    </Col>
+  </Row>
+</div>
 </template>
 <script>
 import http from '../utils/http'
@@ -43,23 +81,149 @@ export default {
       chartData: {
         columns: ['date', 'M0', 'M1', 'M2', 'M3', 'M4'],
         rows: []
+      },
+      chartData_daxiaobi_zuori: {
+        columns: ['item', 'num'],
+        rows: []
+      },
+      chartData_daxiaobi_5ri: {
+        columns: ['date', '0:5', '1:4', '2:3', '3:2', '4:1', '5:0'],
+        rows: []
+      },
+      chartData_qioubi_zuori: {
+        columns: ['item', 'num'],
+        rows: []
+      },
+      chartData_qioubi_5ri: {
+        columns: ['date', '0:5', '1:4', '2:3', '3:2', '4:1', '5:0'],
+        rows: []
       }
     }
   },
-  watch: {
-    // this.$refs[`chart1`].echarts.resize()
+  watch: function () {
+  },
+  created: function () {
+    this.$nextTick(() => {
+      this.$refs.chart1.echarts.resize()
+      this.$refs.chartData_daxiaobi_zuori_ref.echarts.resize()
+      this.$refs.chartData_daxiaobi_5ri_ref.echarts.resize()
+      this.$refs.chartData_qioubi_zuori_ref.echarts.resize()
+      this.$refs.chartData_qioubi_5ri_ref.echarts.resize()
+    })
   },
   mounted: function () {
-    this.get5DayData()
+    this.getDayData()
+    this.getDxbDayData()
+    this.getQobDayData()
   },
   methods: {
-    get5DayData: async function () {
+    getDayData: async function () {
       let params = {
+        day: 15
       }
-      const res = await http.post('/lottery/getTypeCount10Day', params)
+      const res = await http.post('/lottery/getTypeCountDay', params)
       if (http.isSuccess) {
         let data = res.data
         this.chartData.rows = data
+      }
+    },
+    getDxbDayData: async function () {
+      let params = {
+      }
+      const res = await http.post('/lottery/getDxbCountDay', params)
+      if (http.isSuccess) {
+        let data = res.data
+        const dataAll = []
+        const dataZuori = []
+        for (let i = 0; i < data.length; i++) {
+          dataAll.push({
+            'date': data[i].date,
+            '0:5': data[i].item05,
+            '1:4': data[i].item14,
+            '2:3': data[i].item23,
+            '3:2': data[i].item32,
+            '4:1': data[i].item41,
+            '5:0': data[i].item50
+          })
+        }
+        if (data != null && data.length > 0) {
+          dataZuori.push({
+            'item': '0:5',
+            'num': data[0].item05
+          })
+          dataZuori.push({
+            'item': '1:4',
+            'num': data[0].item14
+          })
+          dataZuori.push({
+            'item': '2:3',
+            'num': data[0].item23
+          })
+          dataZuori.push({
+            'item': '3:2',
+            'num': data[0].item32
+          })
+          dataZuori.push({
+            'item': '4:1',
+            'num': data[0].item41
+          })
+          dataZuori.push({
+            'item': '5:0',
+            'num': data[0].item50
+          })
+        }
+        this.chartData_daxiaobi_5ri.rows = dataAll
+        this.chartData_daxiaobi_zuori.rows = dataZuori
+        console.log(dataAll)
+      }
+    },
+    getQobDayData: async function () {
+      let params = {
+      }
+      const res = await http.post('/lottery/getQobCountDay', params)
+      if (http.isSuccess) {
+        let data = res.data
+        const dataAll = []
+        const dataZuori = []
+        for (let i = 0; i < data.length; i++) {
+          dataAll.push({
+            'date': data[i].date,
+            '0:5': data[i].item05,
+            '1:4': data[i].item14,
+            '2:3': data[i].item23,
+            '3:2': data[i].item32,
+            '4:1': data[i].item41,
+            '5:0': data[i].item50
+          })
+        }
+        if (data != null && data.length > 0) {
+          dataZuori.push({
+            'item': '0:5',
+            'num': data[0].item05
+          })
+          dataZuori.push({
+            'item': '1:4',
+            'num': data[0].item14
+          })
+          dataZuori.push({
+            'item': '2:3',
+            'num': data[0].item23
+          })
+          dataZuori.push({
+            'item': '3:2',
+            'num': data[0].item32
+          })
+          dataZuori.push({
+            'item': '4:1',
+            'num': data[0].item41
+          })
+          dataZuori.push({
+            'item': '5:0',
+            'num': data[0].item50
+          })
+        }
+        this.chartData_qioubi_5ri.rows = dataAll
+        this.chartData_qioubi_zuori.rows = dataZuori
       }
     }
   }
--- front/src/main.js
+++ front/src/main.js
@@ -9,6 +9,8 @@ import iView from 'iview'
 // import ElementUI from 'element-ui'
 // import VCharts from 'v-charts'
 import VeLine from 'v-charts/lib/line.common'
+import VeHistogram from 'v-charts/lib/histogram.common'
+import VePie from 'v-charts/lib/pie.common'
 // import 'element-ui/lib/theme-chalk/index.css'
 import 'iview/dist/styles/iview.css'
 import Ads from 'vue-google-adsense'
@@ -23,6 +25,8 @@ Vue.use(Ads.Adsense)
 Vue.use(iView)
 // Vue.use(ElementUI)
 Vue.component(VeLine.name, VeLine)
+Vue.component(VeHistogram.name, VeHistogram)
+Vue.component(VePie.name, VePie)
 
 Vue.config.productionTip = false
 
